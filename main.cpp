28989e42299108f2efe3447f3b007802455ccae4
Tom Freehill <tom.freehill26@gmail.com>
2026-02-03 16:35:21 -0500

    - Added swapchain configuration, and imageviews thereof, to renderer

--- main.cpp
+++ main.cpp
@@ -17,12 +17,20 @@
 #include <optional>
 #include <set>
 
+#include <cstdint> // for uint32_t
+#include <limits> // for std::numeric_limits
+#include <algorithm> // for std::clamp
+
 
 const uint32_t WINDOW_WIDTH = 800;
 const uint32_t WINDOW_HEIGHT = 600;
 
 const std::vector<const char*> VALIDATION_LAYERS = {
 	"VK_LAYER_KHRONOS_validation"
+};
+
+const std::vector<const char*> DEVICE_EXTENSIONS = {
+	VK_KHR_SWAPCHAIN_EXTENSION_NAME
 };
 
 #ifdef NDEBUG
@@ -68,6 +76,12 @@ private:
 		bool IsComplete() const {
 			return graphicsFamily.has_value() && presentFamily.has_value();
 		}
+	};
+
+	struct SwapChainSupportDetails {
+		VkSurfaceCapabilitiesKHR capabilities;
+		std::vector<VkSurfaceFormatKHR> formats;
+		std::vector<VkPresentModeKHR> presentModes;
 	};
 
 public:
@@ -144,7 +158,7 @@ private:
 		return false;
 	}
 
-	std::vector<const char*> GetRequiredExtensions() {
+	std::vector<const char*> GetRequiredInstanceExtensions() {
 		// get which windowing extensions are required
 		uint32_t glfwExtensionCount = 0;
 		const char** glfwExtensionNames;
@@ -158,12 +172,12 @@ private:
 
 		vkEnumerateInstanceExtensionProperties(nullptr, &supportedExtensionCount, supportedExtensions.data());
 
-		std::cout << "glfw required extensions (windows):\n";
+		std::cout << "glfw required instance extensions (windows):\n";
 		for (int i = 0; i < glfwExtensionCount; ++i) {
 			std::cout << "\t" << glfwExtensionNames[i] << (IsExtensionSupported(glfwExtensionNames[i], supportedExtensions) ? " SUPPORTED" : " NOT SUPPORTED") << "\n";
 		}
 
-		std::cout << "available extensions:\n";
+		std::cout << "available instance extensions:\n";
 		for (const auto& supportedExtension : supportedExtensions) {
 			std::cout << "\t" << supportedExtension.extensionName << "\n";
 		}
@@ -234,7 +248,7 @@ private:
 			VK_API_VERSION_1_0					// apiVersion
 		};
 
-		auto requiredExtensions = GetRequiredExtensions();
+		auto requiredExtensions = GetRequiredInstanceExtensions();
 
 		VkInstanceCreateInfo instanceCreateInfo
 		{
@@ -262,6 +276,145 @@ private:
 		// because it influences device selection (eg: multiple screens, etc)
 		CreateSurface(); 
 		PickPhysicalDevice();
+		CreateLogicalDevice();
+
+		// swapchain images are implicitly created when swapchain is created
+		CreateSwapChain();
+		CreateSwapChainImageViews();
+
+		CreateGraphicsPipeline();
+	}
+
+	void CreateGraphicsPipeline() {
+	
+	}
+
+	void CreateSwapChain() {
+		SwapChainSupportDetails swapChainSupport = QuerySwapChainSupport(physicalDevice);
+
+		VkSurfaceFormatKHR surfaceFormat = ChooseSwapSurfaceFormat(swapChainSupport.formats);
+		VkPresentModeKHR presentMode = ChooseSwapPresentMode(swapChainSupport.presentModes);
+		VkExtent2D extent = ChooseSwapExtent(swapChainSupport.capabilities);
+
+		// ensure renderer doesn't need to wait on driver to complete internal operations
+		// before acquiring next image to render to (ie: minimum of double buffering)
+		uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+
+		// maxImageCount == 0 implies no limit
+		if (swapChainSupport.capabilities.maxImageCount > 0
+			&& imageCount > swapChainSupport.capabilities.maxImageCount) {
+			imageCount = swapChainSupport.capabilities.maxImageCount;
+		}
+
+		QueueFamilyIndices indices = FindQueueFamilies(physicalDevice);
+		uint32_t queueFamilyIndices[] = {
+			indices.graphicsFamily.value(),
+			indices.presentFamily.value()
+		};
+
+		// DEBUG: best performance option for sharing, 
+		// but **requires explicit sync/transfer of ownership**
+		VkSharingMode imageSharingMode = VK_SHARING_MODE_EXCLUSIVE; 
+		uint32_t queueFamilyIndexCount = 0;
+		uint32_t* pQueueFamilyIndices = nullptr;
+
+		// check if swapchain images cn be used across queue families
+		if (indices.graphicsFamily != indices.presentFamily) {
+			imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+			queueFamilyIndexCount = 2;
+			pQueueFamilyIndices = queueFamilyIndices;
+		}
+
+		VkSwapchainCreateInfoKHR createInfo{
+			VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,   // sType
+			nullptr,									   // pNext
+			0,											   // flags
+			surface,									   // surface
+			imageCount,									   // minImageCount
+			surfaceFormat.format,						   // imageFormat
+			surfaceFormat.colorSpace,					   // imageColorSpace
+			extent,										   // imageExtent
+			1,											   // imageArrayLayers // TODO (TF 3 FEB 2026): make more than 1 for stereoscopic 3D
+			VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,		   // imageUsage // TODO (TF 3 FEB 2026): test VK_IMAGE_USAGE_TRANSFER_DST_BIT here for post-processing effects into the swapchain images
+			imageSharingMode,							   // imageSharingMode
+			queueFamilyIndexCount,						   // queueFamilyIndexCount
+			pQueueFamilyIndices,						   // pQueueFamilyIndices
+			swapChainSupport.capabilities.currentTransform,// preTransform // TODO (TF 3 FEB 2026): adapt this for rotated and/or flipped setups
+			VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,			   // compositeAlpha // DEBUG: opaque to avoid using alpha to blen with other windows
+			presentMode,								   // presentMode
+			VK_TRUE,									   // clipped // TODO (TF 3 FEB 2026): disable clipping to ensure pixels are readable even if another window obscures the application
+			VK_NULL_HANDLE								   // oldSwapChain // TODO (TF 3 FEB 2026): pass in the old/invalid swapchain if window is resized
+		};
+
+		VkResult result = vkCreateSwapchainKHR(logicalDevice, &createInfo, nullptr, &swapChain);
+		if (result != VK_SUCCESS) {
+			throw std::runtime_error("failed to create swap chain!");
+		}
+
+		// cache swapchain images
+		vkGetSwapchainImagesKHR(logicalDevice, swapChain, &imageCount, nullptr);
+		swapChainImages.resize(imageCount);
+		vkGetSwapchainImagesKHR(logicalDevice, swapChain, &imageCount, swapChainImages.data());
+
+		swapChainImageFormat = surfaceFormat.format;
+		swapChainExtent = extent;
+		// TODO(? TF 3 FEB 2026): cache the surfaceFormat.colorspace too, for XR applications
+	}
+
+	void CreateSwapChainImageViews() {
+		swapChainImageViews.resize(swapChainImages.size());
+
+		for (size_t i = 0; i < swapChainImages.size(); ++i) {
+			VkComponentMapping componentMapping {
+				VK_COMPONENT_SWIZZLE_IDENTITY, // r
+				VK_COMPONENT_SWIZZLE_IDENTITY, // g
+				VK_COMPONENT_SWIZZLE_IDENTITY, // b
+				VK_COMPONENT_SWIZZLE_IDENTITY  // a
+			};
+
+			VkImageSubresourceRange subresourceRange {
+				VK_IMAGE_ASPECT_COLOR_BIT,	// aspectMask // TODO (TF 3 FEB 2026): experiment with Depth and Stencil aspects for views
+				0,							// baseMipLevel
+				1,							// levelCount // TODO (TF 3 FEB 2026): experiment with variable mip levels
+				0,							// baseArraylayer
+				1							// layerCount // TODO (TF 3 FEB 2026): experiment with multiple layers for stereoscopic 3D application
+			};
+
+			VkImageViewCreateInfo createInfo{
+				VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO, // sType
+				nullptr,								  // pNext
+				0,										  // flags
+				swapChainImages[i],						  // image
+				VK_IMAGE_VIEW_TYPE_2D,					  // viewType
+				swapChainImageFormat,					  // format // TODO (TF 3 FEB 2026): experiment with different view formats
+				componentMapping,						  // components // TODO (TF 3 FEB 2026): experiment with monochrome components, or const channels
+				subresourceRange						  // subresourceRange
+			};
+
+			VkResult result = vkCreateImageView(logicalDevice, &createInfo, nullptr, &swapChainImageViews[i]);
+			if (result != VK_SUCCESS) {
+				throw std::runtime_error("failed to create swapchain image views");
+			}
+		}
+	}
+
+	bool CheckDeviceExtensionSupport(VkPhysicalDevice device) {
+		uint32_t extensionCount;
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
+
+		std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+		vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+		std::set<std::string> requiredExtensions(DEVICE_EXTENSIONS.begin(), DEVICE_EXTENSIONS.end());
+		for (const auto& extension : availableExtensions) {
+			uint32_t erasedCount = requiredExtensions.erase(extension.extensionName);
+			if (erasedCount > 0) {
+				std::cout << "found required device extension [" << extension.extensionName << "]" << std::endl;
+			}
+		}
+
+		// all extensions supported if empty
+		return requiredExtensions.empty();
 	}
 
 	// TODO (TF 2 FEB 2026): modify the ranking to fit a unique application
@@ -297,6 +450,25 @@ private:
 			return 0;
 		}
 
+		bool extensionsSupported = CheckDeviceExtensionSupport(device);
+		if (extensionsSupported) {
+			bool swapChainAdequate = false;
+
+			SwapChainSupportDetails swapChainSupport = QuerySwapChainSupport(device);
+
+			// TODO (TF 3 FEB 2026): be more specific in swapchain requirements (min and max)
+			swapChainAdequate = !swapChainSupport.formats.empty()
+								&& !swapChainSupport.presentModes.empty();
+
+			if (!swapChainAdequate) {
+				return 0;
+			}
+		}
+		else {
+			return 0;
+		}
+
+		std::cout << "checking GPU [" << deviceProperties.deviceName << "][" << device << "]\n";
 		return score;
 	}
 
@@ -320,6 +492,7 @@ private:
 
 		if (candidates.crbegin()->first > 0) {
 			physicalDevice = candidates.crbegin()->second;
+			std::cout << "picking GPU [" << physicalDevice << "]" << std::endl;
 		}
 		else {
 			throw std::runtime_error("failed to find a suitable GPU!");
@@ -365,6 +538,76 @@ private:
 		return indices;
 	}
 
+	SwapChainSupportDetails QuerySwapChainSupport(VkPhysicalDevice device) {
+		SwapChainSupportDetails details;
+		uint32_t formatCount;
+		uint32_t presentModeCount;
+
+		vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, surface, &details.capabilities);
+		vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, nullptr);
+		vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, nullptr);
+
+		if (formatCount > 0) {
+			details.formats.resize(formatCount);
+			vkGetPhysicalDeviceSurfaceFormatsKHR(device, surface, &formatCount, details.formats.data());
+		}
+
+		if (presentModeCount > 0) {
+			details.presentModes.resize(presentModeCount);
+			vkGetPhysicalDeviceSurfacePresentModesKHR(device, surface, &presentModeCount, details.presentModes.data());
+		}
+
+		return details;
+	}
+
+	// TODO (TF 3 FEB 2026): select different format and colorspace combination as needed (not hardcoded)
+	VkSurfaceFormatKHR ChooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+		for (const auto& availableFormat : availableFormats) {
+			if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB
+				&& availableFormat.colorSpace == VK_COLORSPACE_SRGB_NONLINEAR_KHR) {
+				return availableFormat;
+			}
+		}
+
+		return availableFormats[0]; // TODO (TF 3 FEB 2026): don't default to the first surfaceFormat available
+	}
+
+	// TODO (TF 3 FEB 2026): explore different modes (minimize tearing)
+	VkPresentModeKHR ChooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+		for (const auto& availablePresentMode : availablePresentModes) {
+			if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+				return availablePresentMode;
+			}
+		}
+
+		return VK_PRESENT_MODE_FIFO_KHR; // FIFO guaranteed to be present by vulkan
+	}
+
+	VkExtent2D ChooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+		// max uint32 would indicate currentExtent can be customized
+		if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+			return capabilities.currentExtent; 
+		}
+
+		int width;
+		int height;
+		glfwGetFramebufferSize(window, &width, &height); // extent in pixel units
+
+		VkExtent2D actualExtent = {
+			static_cast<uint32_t>(width),
+			static_cast<uint32_t>(height)
+		};
+
+		actualExtent.width = std::clamp(actualExtent.width,
+										capabilities.minImageExtent.width,
+										capabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(actualExtent.height,
+										capabilities.minImageExtent.height,
+										capabilities.maxImageExtent.height);
+
+		return actualExtent;
+	}
+
 	void CreateLogicalDevice() {
 		QueueFamilyIndices indices = FindQueueFamilies(physicalDevice);
 
@@ -393,16 +636,17 @@ private:
 			// TODO (TF 2 FEB 2026): left all VK_FALSE for now
 		}; 
 
-		uint32_t enabledExtensionCount = 0;
-		uint32_t enabledLayerCount = 0;
-		const char* const* ppEnabledLayerNames = nullptr;
-
 		// Logical device layers are deprecated in favor of instance layers
 		// this is only here for legacy support
+		uint32_t enabledLayerCount = 0;
+		const char* const* ppEnabledLayerNames = nullptr;
 		if (enableValidationLayers) {
 			enabledLayerCount = static_cast<uint32_t>(VALIDATION_LAYERS.size());
 			ppEnabledLayerNames = VALIDATION_LAYERS.data();
 		}
+
+		uint32_t enabledExtensionCount = static_cast<uint32_t>(DEVICE_EXTENSIONS.size());
+		const char* const* ppEnabledExtensionNames = DEVICE_EXTENSIONS.data();
 
 		VkDeviceCreateInfo createInfo {
 			VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,			// sType
@@ -413,7 +657,7 @@ private:
 			enabledLayerCount,								// enabledLayerCount
 			ppEnabledLayerNames,							// ppEnabledLayerNames
 			enabledExtensionCount,							// enabledExtensionCount
-			nullptr,										// ppEnabledExtensionNames // TODO (TF 2 FEB 2025) use an extension (eg "VK_KHR_swapchain")
+			ppEnabledExtensionNames,						// ppEnabledExtensionNames
 			&deviceFeatures									// pEnabledFeatures
 		};
 
@@ -469,6 +713,13 @@ private:
 	}
 
 	void Cleanup() {
+		for (auto imageView : swapChainImageViews) {
+			vkDestroyImageView(logicalDevice, imageView, nullptr);
+		}
+
+		// swapchain images are implicitly cleaned up when the swapchain is destroyed
+		vkDestroySwapchainKHR(logicalDevice, swapChain, nullptr);
+
 		// device queues are implicitly cleaned up when the devices is destroyed
 		vkDestroyDevice(logicalDevice, nullptr);
 
@@ -497,6 +748,11 @@ private:
 	VkSurfaceKHR surface;
 	GLFWwindow* window;
 
+	VkSwapchainKHR swapChain;
+	std::vector<VkImage> swapChainImages;
+	VkFormat swapChainImageFormat;
+	VkExtent2D swapChainExtent;
+	std::vector<VkImageView> swapChainImageViews;
 };
 
 int main() {
